9be2883c93c113f96422a777047131b5468281fd
Carlos Hernando Garnica Romero <117132980+Solrac26@users.noreply.github.com>
2024-07-07 22:54:29 -0500

    Update app.js
    Pequeñas Mejoras.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = 0; // Variable para almacenar el número secreto generado aleatoriamente
+// Declaración de variables globales
+let numeroSecreto = 0; // Almacena el número secreto generado aleatoriamente
 let intentos = 0; // Contador de intentos del usuario
 let listaNumerosSorteados = []; // Lista para almacenar los números secretos ya sorteados
 let numeroMaximo = 10; // Número máximo hasta el cual se puede generar el número secreto (exclusivo)
@@ -7,37 +8,39 @@ let numeroMaximo = 10; // Número máximo hasta el cual se puede generar el núm
 function asignarTextoElemento(elemento, texto) {
   let elementoHTML = document.querySelector(elemento);
   elementoHTML.innerHTML = texto;
-  return;
 }
 
 // Función para verificar el intento del usuario
 function verificarIntento() {
   let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value); // Obtener el número ingresado por el usuario como entero
  
-  if (intentos < 4) { // Si los intentos no han alcanzado el límite máximo (4 intentos)
-    if (numeroDeUsuario === numeroSecreto) { // Si el usuario adivinó el número secreto
+  // Verificar si los intentos del usuario no han alcanzado el límite máximo (4 intentos)
+  if (intentos < 4) {
+    // Si el usuario adivinó el número secreto
+    if (numeroDeUsuario === numeroSecreto) {
       asignarTextoElemento(
         "p",
         "Acertaste el numero en " +
           intentos +
           (intentos === 1 ? " vez." : " veces.")
       );
-      document.getElementById("reiniciar").removeAttribute("disabled"); // Habilitar el botón para reiniciar el juego
+      // Habilitar el botón para reiniciar el juego
+      document.getElementById("reiniciar").removeAttribute("disabled");
     } else {
-      // El usuario no acertó
+      // Si el número ingresado por el usuario es menor que el número secreto
       if (numeroDeUsuario < numeroSecreto) {
         asignarTextoElemento("p", `El numero Secreto es Mayor, llevas: ${intentos} ${intentos == 1 ? 'intento.' : 'intentos.'}`);
       } else {
+        // Si el número ingresado por el usuario es mayor que el número secreto
         asignarTextoElemento("p", `El numero Secreto es Menor, llevas: ${intentos} ${intentos == 1 ? 'intento.' : 'intentos.'}`);
       }
       intentos++; // Incrementar el contador de intentos
       limpiarCaja(); // Limpiar el campo de entrada del usuario
     }
   } else {
-    // El usuario alcanzó el límite de intentos
+    // Si el usuario alcanzó el límite de intentos
     asignarTextoElemento("p",'Llegaste al limite de Intentos.');
   }
-  return;
 }
 
 // Función para limpiar el campo de entrada del usuario
@@ -57,7 +60,7 @@ function condicionesIniciales() {
 function reiniciarJuego() {
   limpiarCaja(); // Limpiar el campo de entrada del usuario
   condicionesIniciales(); // Restablecer las condiciones iniciales del juego
-  document.querySelector("#reiniciar").setAttribute("disabled", "true"); // Deshabilitar el botón de reiniciar juego
+  document.getElementById("reiniciar").setAttribute("disabled", "true"); // Deshabilitar el botón de reiniciar juego
 }
 
 // Función para generar un número secreto aleatorio
@@ -68,8 +71,9 @@ function generarNumeroSecreto() {
   if (listaNumerosSorteados.length == numeroMaximo) {
     asignarTextoElemento("p", `Ya se sortearon todos los numeros posibles.`);
   } else {
+    // Si el número generado está en la lista de números sorteados, generar otro número
     if (listaNumerosSorteados.includes(numeroGenerado)) {
-      return generarNumeroSecreto(); // Si el número ya está en la lista, generar otro número
+      return generarNumeroSecreto();
     } else {
       listaNumerosSorteados.push(numeroGenerado); // Agregar el número generado a la lista de números sorteados
       return numeroGenerado; // Devolver el número generado como número secreto
@@ -78,6 +82,8 @@ function generarNumeroSecreto() {
 }
 
 condicionesIniciales(); // Llamar a la función condicionesIniciales para iniciar el juego
+
+
 
 
 
